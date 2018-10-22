b5571d28f129cd4ae5bc8ad237ae28bf5132de33
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-10-22 23:22:11 +0100

    Issue #1: Fix pylint.

--- sksurgeryimage/processing/interlace.py
+++ sksurgeryimage/processing/interlace.py
@@ -1,62 +1,70 @@
 # coding=utf-8
 
-"""Functions to support de-interlacing and re-interlacing of 2D video frames."""
+"""Functions to support deinterlacing and reinterlacing of 2D video frames."""
 
 import numpy as np
 
 
 def validate_interlaced_image_sizes(even_rows, odd_rows, interlaced):
     """
-    Validates the relative sizes of the even_rows, odd_rows and interlaced images.
+    Validates the sizes of the even_rows, odd_rows and interlaced images.
 
-    1. Inputs must all be numpy images, or throw TypeError.
-    2. Must all be the same width, or throw ValueError.
-    3. Must all have an even number of rows, or throw ValueError.
-    4. even_rows and odd_rows must have the same number of rows, or throw ValueError.
-    5. even_rows and odd_rows must have half the number of rows as interlaced, or throw ValueError.
+    1. Inputs must all be numpy images.
+    2. Must all be the same width.
+    3. Must all have an even number of rows.
+    4. even_rows and odd_rows must have the same number of rows.
+    5. even_rows and odd_rows must have half the number of rows as interlaced.
 
-    :param even_rows: numpy image array, with even number of rows, for example 540 (rows) x 1920 (columns).
-    :param odd_rows: numpy image array, with even number of rows, for example 540 (rows) x 1920 (columns).
-    :param interlaced: numpy image array, with even number of rows, for example 1080 (rows) x 1920 (columns).
+    :param even_rows: numpy image array, with even number of rows.
+    :param odd_rows: numpy image array, with even number of rows.
+    :param interlaced: numpy image array, with even number of rows.
     :return: nothing
     """
 
     if not isinstance(even_rows, np.ndarray):
-        raise TypeError('even_rows is not a numpy array')
+        raise TypeError("even_rows is not a numpy array")
 
     if not isinstance(odd_rows, np.ndarray):
-        raise TypeError('odd_rows is not a numpy array')
+        raise TypeError("odd_rows is not a numpy array")
 
     if not isinstance(interlaced, np.ndarray):
-        raise TypeError('interlaced is not a numpy array')
+        raise TypeError("interlaced is not a numpy array")
 
     if even_rows.shape[1] != odd_rows.shape[1]:
-        raise ValueError("The even_rows image should have the same number of columns as the odd_rows image")
+        raise ValueError("even_rows should have the same number "
+                         + "of columns odd_rows")
 
     if odd_rows.shape[1] != interlaced.shape[1]:
-        raise ValueError("The odd_rows image should have the same number of columns as the interlaced image")
+        raise ValueError("odd_rows should have the same number of "
+                         + "columns as interlaced")
 
     if even_rows.shape[0] % 2 != 0:
-        raise ValueError("The even_rows image should have an even number of rows.")
+        raise ValueError("even_rows should have an even number of rows")
 
     if odd_rows.shape[0] % 2 != 0:
-        raise ValueError("The odd_rows image should have an even number of rows.")
+        raise ValueError("odd_rows should have an even number of rows")
 
     if interlaced.shape[0] % 2 != 0:
-        raise ValueError("The interlaced image should have an even number of rows.")
+        raise ValueError("interlaced should have an even number of rows")
 
     if even_rows.shape[0] != odd_rows.shape[0]:
-        raise ValueError("The even_rows image should have the same number of rows as the odd_rows image")
+        raise ValueError("even_rows should have the same number of rows "
+                         + "as odd_rows")
 
     if even_rows.shape[0] * 2 != interlaced.shape[0]:
-        raise ValueError("The even_rows output image should have half the number of rows as the interlaced image.")
+        raise ValueError("even_rows output image should have half the number "
+                         + "of rows as interlaced")
 
     if odd_rows.shape[0] * 2 != interlaced.shape[0]:
-        raise ValueError("The odd_rows output image should have half the number of rows as the interlaced image.")
+        raise ValueError("odd_rows should have half the number of rows as "
+                         + "interlaced")
 
 
 def interlace_preallocated_images(even_rows, odd_rows, interlaced):
-
+    """
+    Interlaces even_rows and odd_rows images into the interlaced image,
+    assuming all inputs are pre-allocated.
+    """
     validate_interlaced_image_sizes(even_rows, odd_rows, interlaced)
 
     interlaced[0::2] = even_rows
@@ -64,7 +72,9 @@ def interlace_preallocated_images(even_rows, odd_rows, interlaced):
 
 
 def interlace(even_rows, odd_rows):
-
+    """
+    Interlaces even_rows and odd_rows images into a new output image.
+    """
     if not isinstance(even_rows, np.ndarray):
         raise TypeError('even_rows is not a numpy array')
 
@@ -81,7 +91,10 @@ def interlace(even_rows, odd_rows):
 
 
 def deinterlace_preallocated_images(interlaced, even_rows, odd_rows):
-
+    """
+    Deinterlaces the interlaced image into even_rows and odd_rows images,
+    assuming that all images are pre-allocated.
+    """
     validate_interlaced_image_sizes(even_rows, odd_rows, interlaced)
 
     even_rows[:, :, :] = interlaced[0::2]
@@ -89,11 +102,15 @@ def deinterlace_preallocated_images(interlaced, even_rows, odd_rows):
 
 
 def deinterlace(interlaced):
-
+    """
+    Takes the input image, and splits into an image of even_rows and odd_rows.
+    """
     if not isinstance(interlaced, np.ndarray):
         raise TypeError('interlaced is not a numpy array')
 
-    output_dims = (interlaced.shape[0]//2, interlaced.shape[1], interlaced.shape[2])
+    output_dims = (interlaced.shape[0]//2,
+                   interlaced.shape[1],
+                   interlaced.shape[2])
 
     even_rows = np.empty(output_dims, dtype=interlaced.dtype)
     odd_rows = np.empty(output_dims, dtype=interlaced.dtype)
