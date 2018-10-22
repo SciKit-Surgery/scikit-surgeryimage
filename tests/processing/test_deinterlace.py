bbf6f7eed822390ad2486f9e3139e4689a46fe97
t <tdowrick@gmail.com>
2018-10-22 15:31:06 +0100

    Issue #1: Initial unit tests for interlace/deinterlace

---
+++ tests/processing/test_deinterlace.py
@@ -0,0 +1,80 @@
+import pytest
+import numpy as np
+from sksurgeryimage.processing import interlace
+
+@pytest.fixture
+def create_valid_input():
+
+    def _create_valid_input(rows, cols):
+
+        dims = (rows, cols, 3)
+
+        odd_row_value = 0
+        even_row_value = 1
+
+        interlaced = np.empty(dims, dtype=np.uint8)
+
+        for idx in range(rows):
+            if idx % 2:
+                interlaced[idx,:,:] = odd_row_value
+            else:
+                interlaced[idx,:,:] = even_row_value
+
+        return interlaced
+    
+    return _create_valid_input
+
+
+def test_invalid_input():
+    
+    interlaced = None
+    assert interlace.deinterlace(interlaced) == False
+
+
+def test_missized_input(create_valid_input):
+
+    rows = 21
+    cols = 10
+
+    interlaced = create_valid_input(rows, cols)
+
+    assert interlace.deinterlace(interlaced) == False
+
+
+                
+
+def test_small_input(create_valid_input):
+
+    rows = 20
+    cols = 10
+
+    interlaced = create_valid_input(rows, cols)
+    
+    left, right = interlace.deinterlace(interlaced)
+
+    output_dims = (rows//2, cols, 3)
+
+    expected_left = np.ones(output_dims, dtype=np.uint8)
+    expected_right = np.zeros(output_dims, dtype=np.uint8)
+
+    np.testing.assert_array_equal(left, expected_left)
+    np.testing.assert_array_equal(right, expected_right)
+
+
+def test_big_input(create_valid_input):
+
+    rows = 1080 * 2
+    cols = 1920
+
+    interlaced = create_valid_input(rows, cols)
+
+    left, right = interlace.deinterlace(interlaced)
+
+    output_dims = (rows//2, cols, 3)
+
+    expected_left = np.ones(output_dims, dtype=np.uint8)
+    expected_right = np.zeros(output_dims, dtype=np.uint8)
+
+    np.testing.assert_array_equal(left, expected_left)
+    np.testing.assert_array_equal(right, expected_right)
+
---
+++ tests/processing/test_interlace.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pytest
+from sksurgeryimage.processing import interlace
+
+
+@pytest.fixture
+def create_valid_inputs():
+
+    def _create_valid_inputs(rows, cols):
+        dims = (rows, cols, 3)
+
+        left = np.ones(dims, dtype=np.uint8)
+        right = np.zeros(dims, dtype=np.uint8)
+
+        return (left, right)
+
+    return _create_valid_inputs
+
+
+def test_empty_inputs():
+
+    dims = (10,10,3)
+
+    left = None
+    right = np.ones(dims)
+
+    assert interlace.interlace(left,right) == False
+
+
+def test_inputs_arent_same_size():
+
+    left_dims = (10,10,3)
+    right_dims = (20,20,3)
+
+    left = np.ones(left_dims)
+    right =  np.zeros(right_dims)
+
+    assert interlace.interlace(left,right) == False
+
+
+def test_small_inputs(create_valid_inputs):
+
+    rows = 10
+    cols = 10
+
+    left, right = create_valid_inputs(rows, cols)
+    interlaced = interlace.interlace(left, right)
+
+    for idx in range(rows):
+        left_idx = idx * 2
+        right_idx = left_idx + 1
+
+        np.testing.assert_array_equal(interlaced[left_idx,:,:], left[idx,:,:])
+        np.testing.assert_array_equal(interlaced[right_idx,:,:], right[idx,:,:])
+     
+ 
+def test_HD_inputs(create_valid_inputs):
+
+    rows = 1920
+    cols = 1080
+
+    left, right = create_valid_inputs(rows, cols)
+    interlaced = interlace.interlace(left, right)
+
+    for idx in range(rows):
+        left_idx = idx * 2
+        right_idx = left_idx + 1
+
+        np.testing.assert_array_equal(interlaced[left_idx,:,:], left[idx,:,:])
+        np.testing.assert_array_equal(interlaced[right_idx,:,:], right[idx,:,:])
+
