3eac66bd72d7435eacc4d3ca878cf637f63a61d8
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-10-22 22:26:10 +0100

    Issue #1: Added function to validate size of images for interlacing.

--- sksurgeryimage/processing/interlace.py
+++ sksurgeryimage/processing/interlace.py
@@ -1,5 +1,48 @@
 import numpy as np
 
+
+def validate_interlaced_image_sizes(even_rows, odd_rows, interlaced):
+    """
+    Validates the relative sizes of the even_rows, odd_rows and interlaced images.
+
+    1. Must all be the same width.
+    2. Must all have an even number of rows.
+    3. even_rows and odd_rows must have the same number of rows.
+    4. even_rows and odd_rows must have half the number of rows as interlaced.
+
+    Failures are indicated by throwing ValueError.
+
+    :param even_rows: numpy image array, with even number of rows, for example 540 (rows) x 1920 (columns).
+    :param odd_rows: numpy image array, with even number of rows, for example 540 (rows) x 1920 (columns).
+    :param interlaced: numpy image array, with even number of rows, for example 1080 (rows) x 1920 (columns).
+    :return: nothing
+    """
+
+    if even_rows.shape[1] != odd_rows.shape[1]:
+        raise ValueError("The even_rows image should have the same number of columns as the odd_rows image")
+
+    if odd_rows.shape[1] != interlaced.shape[1]:
+        raise ValueError("The odd_rows image should have the same number of columns as the interlaced image")
+
+    if even_rows.shape[0] % 2 != 0:
+        raise ValueError("The even_rows image should have an even number of rows.")
+
+    if odd_rows.shape[0] % 2 != 0:
+        raise ValueError("The odd_rows image should have an even number of rows.")
+
+    if interlaced.shape[0] % 2 != 0:
+        raise ValueError("The interlaced image should have an even number of rows.")
+
+    if even_rows.shape[0] != odd_rows.shape[0]:
+        raise ValueError("The even_rows image should have the same number of rows as the odd_rows image")
+
+    if even_rows.shape[0] * 2 != interlaced.shape[0]:
+        raise ValueError("The even_rows output image should have half the number of rows as the interlaced image.")
+
+    if odd_rows.shape[0] * 2 != interlaced.shape[0]:
+        raise ValueError("The odd_rows output image should have half the number of rows as the interlaced image.")
+
+
 def interlace(left, right):
 
     if not isinstance(left, np.ndarray):
