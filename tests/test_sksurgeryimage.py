b402c0c9c708417cf12209177c52352ab3c1b24a
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-10-23 09:30:57 +0100

    Issue #1: Remove unittest style unit test, as we use pytest.

--- tests/test_sksurgeryimage.py
+++ tests/test_sksurgeryimage.py
@@ -4,16 +4,6 @@
 
 from sksurgeryimage.ui.sksurgeryimage_demo import run_demo
 
-# Unittest style test case
-from unittest import TestCase
-
-class Testsksurgeryimage(TestCase):
-    def test_using_unittest_sksurgeryimage(self):
-        return_value = run_demo(True, "Hello world")
-        self.assertTrue(return_value)
-
-
-# Pytest style
 
 def test_using_pytest_sksurgeryimage():
     assert run_demo(True, "Hello World") == True
