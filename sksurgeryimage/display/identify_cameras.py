6285ada1d7cbff879743840d389321237859132e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-11-10 13:40:15 +0000

    Issue #3: Fixup linting.

--- sksurgeryimage/display/identify_cameras.py
+++ sksurgeryimage/display/identify_cameras.py
@@ -1,11 +1,23 @@
 # coding=utf-8
 
+"""
+Various utility functions, not for use outside this module.
+These may be removed at any time.
+"""
+
+import logging
+import cv2
+import sksurgeryimage.utilities.camera_utilities as cu
+import sksurgeryimage.utilities.utilities as u
+
+LOGGER = logging.getLogger(__name__)
+
 
 def identify_cameras():
     """
     Show images from each camera, with the camera input number overlaid.
     """
-    num_cameras = count_cameras()
+    num_cameras = cu.count_cameras()
     cameras = []
 
     for i in range(num_cameras):
@@ -18,17 +30,14 @@ def identify_cameras():
             title_string = "Camera: " + str(i) + ". Press q to quit."
 
             if ret:
-                text_overlay_properties = prepare_cv2_text_overlay(i, frame)
+                text_overlay_properties = u.prepare_cv2_text_overlay(i, frame)
                 cv2.putText(frame, *text_overlay_properties)
-
                 cv2.imshow(title_string, frame)
-
             else:
-                logging.info("Camera %s not grabbing", i)
+                LOGGER.info("Camera %s not grabbing", i)
 
         if cv2.waitKey(1) == ord('q'):
             break
 
     for i in range(num_cameras):
         cameras[i].release()
-
