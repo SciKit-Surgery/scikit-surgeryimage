389b1794355ca1d498631ef19a2675c355276c17
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-12-03 09:59:02 +0000

    Issue #17: Add validation functions to check image dimension, camera matrices and distortion coefficients.

--- sksurgeryimage/utilities/utilities.py
+++ sksurgeryimage/utilities/utilities.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 
 """
-Various utilities, like checking strings, numbers, preparing overlay text etc.
+Various utilities, like preparing overlay text, validating image sizes,
+camera matrix sizes, distortion coefficient sizes etc.
 """
+import numpy as np
 import os
 import cv2
 
@@ -27,6 +29,7 @@ def prepare_cv2_text_overlay(overlay_text, frame, text_scale=1):
 def validate_text_input(overlay_text):
     """
     Raises an error if input isn't a string or number.
+
     :raises: TypeError
     """
     if not is_string_or_number(overlay_text):
@@ -53,3 +56,52 @@ def validate_file_input(file_input):
         return True
 
     raise ValueError('Input file:' + file_input + ' does not exist')
+
+
+def validate_width_height(dims):
+    """
+    Checks if dims (width, height) is a valid specification,
+    meaning width and height are both integers above zero.
+
+    :param dims: (width, height)
+    :raises: TypeError, ValueError
+    """
+    if dims is None:
+        raise ValueError("Null input for (width, height).")
+
+    width, height = dims
+
+    if not isinstance(width, int):
+        raise TypeError("Width should be an integer.")
+
+    if not isinstance(height, int):
+        raise TypeError("Height should be an integer.")
+
+    if width < 1:
+        raise ValueError("Width should be >= 1.")
+
+    if height < 1:
+        raise ValueError("Height should be >= 1.")
+
+
+def validate_camera_matrix(matrix):
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("Camera matrix is not a numpy ndarray.")
+    if len(matrix.shape) != 2:
+        raise ValueError("Camera matrix should have 2 dimension.")
+    if matrix.shape[0] != 3:
+        raise ValueError("Camera matrix should have 3 rows.")
+    if matrix.shape[1] != 3:
+        raise ValueError("Camera matrix should have 3 columns.")
+
+
+def validate_distortion_coefficients(matrix):
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("Distortion coefficients are not a numpy ndarray.")
+    if len(matrix.shape) != 2:
+        raise ValueError("Camera matrix should have 2 dimension.")
+    if matrix.shape[0] != 1:
+        raise ValueError("Distortion coefficients should have 1 rows.")
+    if matrix.shape[1] not in [4, 5, 8, 12, 14]:  # See OpenCV docs
+        raise ValueError("Distortion coefficients should have 4, 5, 8, 12 or 14 columns.")
+
--- tests/utilities/test_utilities.py
+++ tests/utilities/test_utilities.py
@@ -51,3 +51,89 @@ def test_prepare_text_overlay():
         "1", (0, frame_dims[0] - 10), cv2.FONT_HERSHEY_COMPLEX, scale, (255, 255, 255))
 
     assert expected_large == large_text_overlay_properties
+
+
+def test_width_height_invalid_because_none_input():
+    with pytest.raises(ValueError):
+        utilities.validate_width_height(None)
+
+
+def test_width_height_invalid_because_empty_input():
+    with pytest.raises(ValueError):
+        utilities.validate_width_height(())
+
+
+def test_width_height_invalid_because_width_wrong_type():
+    with pytest.raises(TypeError):
+        utilities.validate_width_height(("", 2))
+
+
+def test_width_height_invalid_because_width_too_low():
+    with pytest.raises(ValueError):
+        utilities.validate_width_height((0, 2))
+
+
+def test_width_height_invalid_because_height_wrong_type():
+    with pytest.raises(TypeError):
+        utilities.validate_width_height((1, ""))
+
+
+def test_width_height_invalid_because_height_too_low():
+    with pytest.raises(ValueError):
+        utilities.validate_width_height((1, 0))
+
+
+def test_camera_matrix_invalid_because_wrong_type():
+    with pytest.raises(TypeError):
+        utilities.validate_camera_matrix(1)
+
+
+def test_camera_matrix_invalid_because_not_two_dimensional():
+    with pytest.raises(ValueError):
+        utilities.validate_camera_matrix(np.ones((3, 3, 3)))
+
+
+def test_camera_matrix_invalid_because_too_few_rows():
+    with pytest.raises(ValueError):
+        utilities.validate_camera_matrix(np.ones((1, 3)))
+
+
+def test_camera_matrix_invalid_because_too_many_rows():
+    with pytest.raises(ValueError):
+        utilities.validate_camera_matrix(np.ones((4, 3)))
+
+
+def test_camera_matrix_invalid_because_too_few_columns():
+    with pytest.raises(ValueError):
+        utilities.validate_camera_matrix(np.ones((3, 1)))
+
+
+def test_camera_matrix_invalid_because_too_many_columns():
+    with pytest.raises(ValueError):
+        utilities.validate_camera_matrix(np.ones((3, 4)))
+
+
+def test_distortion_coefficients_invalid_because_wrong_type():
+    with pytest.raises(TypeError):
+        utilities.validate_distortion_coefficients(1)
+
+
+def test_distortion_coefficients_invalid_because_not_two_dimensional():
+    with pytest.raises(ValueError):
+        utilities.validate_distortion_coefficients(np.ones((3, 3, 3)))
+
+
+def test_distortion_coefficients_invalid_because_too_many_rows():
+    with pytest.raises(ValueError):
+        utilities.validate_distortion_coefficients(np.ones((3, 4)))
+
+
+def test_distortion_coefficients_invalid_because_too_few_columns():
+    with pytest.raises(ValueError):
+        utilities.validate_distortion_coefficients(np.ones((1, 3)))
+
+
+def test_distortion_coefficients_invalid_because_number_of_columns_not_in_list():
+    with pytest.raises(ValueError):
+        # Should accept [4, 5, 8, 12, 14]
+        utilities.validate_distortion_coefficients(np.ones((1, 6)))
