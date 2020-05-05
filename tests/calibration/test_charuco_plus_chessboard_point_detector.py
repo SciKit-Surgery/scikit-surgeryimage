56339e4daed7e2230cbaf7879565089160298cee
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-05-05 19:46:44 +0100

    Issue #75: Increase test coverage by covering ChArUcoPlusChessboard generator.

--- tests/calibration/test_charuco_plus_chessboard_point_detector.py
+++ tests/calibration/test_charuco_plus_chessboard_point_detector.py
@@ -9,26 +9,31 @@ import pytest
 import numpy as np
 from sksurgeryimage.calibration.charuco_plus_chessboard_point_detector import CharucoPlusChessboardPointDetector
 import sksurgeryimage.calibration.point_detector_utils as pdu
+import sksurgeryimage.calibration.charuco as ch
 
 
 def test_charuco_plus_chess_detector():
 
-    # Note: image generated with
-    # calib_image = ch.make_charuco_with_chessboard(chessboard_squares=(9, 14))
-    # cv2.imwrite(file_name, calib_image)
+    generated_image = ch.make_charuco_with_chessboard()
+    output_file_name = 'tests/output/pattern_4x4_19x26_5_4_with_inset_9x14.png'
+    cv2.imwrite(output_file_name, generated_image)
+    generated_image = cv2.imread(output_file_name)
 
-    file_name = 'tests/data/calibration/pattern_4x4_19x26_5_4_with_inset_9x14.png'
-    image = cv2.imread(file_name)
+    input_file_name = 'tests/data/calibration/pattern_4x4_19x26_5_4_with_inset_9x14.png'
+    reference_image = cv2.imread(input_file_name)
+
+    assert np.allclose(reference_image, generated_image)
+
     detector = CharucoPlusChessboardPointDetector()
-    ids_portrait, object_points_portrait, image_points_portrait = detector.get_points(image)
+    ids_portrait, object_points_portrait, image_points_portrait = detector.get_points(reference_image)
     if ids_portrait.shape[0] > 0:
-        pdu.write_annotated_image(image, ids_portrait, image_points_portrait, file_name)
+        pdu.write_annotated_image(reference_image, ids_portrait, image_points_portrait, input_file_name)
 
-    file_name = 'tests/data/calibration/pattern_4x4_19x26_5_4_with_inset_9x14_landscape.png'
-    image = cv2.imread(file_name)
+    input_file_name = 'tests/data/calibration/pattern_4x4_19x26_5_4_with_inset_9x14_landscape.png'
+    image = cv2.imread(input_file_name)
     ids_landscape, object_points_landscape, image_points_landscape = detector.get_points(image)
     if ids_landscape.shape[0] > 0:
-        pdu.write_annotated_image(image, ids_landscape, image_points_landscape, file_name)
+        pdu.write_annotated_image(image, ids_landscape, image_points_landscape, input_file_name)
 
     assert ids_portrait.shape[0] == 468
     assert object_points_portrait.shape[0] == 468
