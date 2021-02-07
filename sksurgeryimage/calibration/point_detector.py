66f7a0214d711a4b66d6f29f590f5deaf9e1388f
Matt Clarkson <m.clarkson@ucl.ac.uk>
2021-02-07 10:37:16 +0000

    Issue #91: Add base-class methods to PointDetector to set camera matrices and distortion coefficients if derived classes need them.

--- sksurgeryimage/calibration/point_detector.py
+++ sksurgeryimage/calibration/point_detector.py
@@ -7,21 +7,89 @@ e.g. Chessboard corners, SIFT points, Charuco points etc.
 """
 
 import logging
+import copy
 import numpy as np
 import cv2
 
 LOGGER = logging.getLogger(__name__)
 
 
+def _validate_camera_parameters(camera_intrinsics,
+                                distortion_coefficients):
+    """
+    Validates that the camera intrinsics are the correct shape.
+    Throws ValueError if incorrect.
+
+    :param camera_intrinsics: [3x3] matrix
+    :param distortion_coefficients: [1xn] matrix
+    """
+    if camera_intrinsics is None:
+        raise ValueError('camera_intrinsics is None')
+    if distortion_coefficients is None:
+        raise ValueError('distortion_coefficients is None')
+    if camera_intrinsics.shape[0] != 3:
+        raise ValueError('camera_intrinsics does not have 3 rows')
+    if camera_intrinsics.shape[1] != 3:
+        raise ValueError('camera_intrinsics does not have 3 columns')
+    if distortion_coefficients.shape[0] != 1:
+        raise ValueError('distortion_coefficients does not have 1 row')
+
+
 class PointDetector:
     """
     Class to detect points in a 2D video image.
+
+    These point detectors are often used to detect points for camera
+    calibration. However, it would also be possible for some subclasses
+    to utilise camera intrinsics and distortion coefficients in order
+    to improve the point detection process itself. It would be up to the
+    derived class to decide how to use them, if at all.
+
     :param scale: tuple (x scale, y scale) to scale up/down the image
+    :param camera_intrinsics: [3x3] camera matrix
+    :param distortion_coefficients: [1xn] distortion coefficients
     """
-    def __init__(self, scale=(1, 1)):
+    def __init__(self,
+                 scale=(1, 1),
+                 camera_intrinsics=None,
+                 distortion_coefficients=None):
 
         self.scale = scale
         self.scale_x, self.scale_y = scale
+        self.camera_intrinsics = None
+        self.distortion_coefficients = None
+        if camera_intrinsics is not None and \
+                distortion_coefficients is not None:
+            self.set_camera_parameters(camera_intrinsics,
+                                       distortion_coefficients)
+
+    def set_camera_parameters(self,
+                              camera_intrinsics,
+                              distortion_coefficients):
+        """
+        Enables camera parameters to be set dynamically at run-time.
+
+        :param camera_intrinsics: [3x3] camera matrix
+        :param distortion_coefficients: [1xn] distortion coefficients
+        """
+        _validate_camera_parameters(camera_intrinsics,
+                                    distortion_coefficients)
+        self.camera_intrinsics = copy.deepcopy(camera_intrinsics)
+        self.distortion_coefficients = copy.deepcopy(distortion_coefficients)
+
+    def get_camera_parameters(self):
+        """
+        Returns a copy of the camera matrix, and distortion coefficients.
+        Throws RuntimeError if either are None.
+
+        :return: [3x3], [1xn] matrices
+        """
+        if self.camera_intrinsics is None or \
+                self.distortion_coefficients is None:
+            raise RuntimeError("Camera parameters are not set")
+        tmp_ci = copy.deepcopy(self.camera_intrinsics)
+        tmp_dc = copy.deepcopy(self.distortion_coefficients)
+        return tmp_ci, tmp_dc
 
     def get_points(self, image, is_distorted=True):
         """
