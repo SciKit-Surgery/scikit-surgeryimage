9768f77d18201cc49349d844807b72179f23a617
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-01-12 11:53:56 +0000

    Issue #104 went headless

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
 
     install_requires=[
         'numpy>=1.11',
-        'opencv-contrib-python>=4.1.1.26',
+        'opencv-contrib-python-headless>=4.1.1.26',
         'scikit-surgerycore>=0.1.0',
     ],
 
