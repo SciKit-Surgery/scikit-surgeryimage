caf6d9da97745c677c35406b59ccc12381083c18
Matt Clarkson <m.clarkson@ucl.ac.uk>
2026-03-07 16:15:25 +0000

    Issue #116: Fixup existing unit tests, minor different, one or at most a few extra points detected.

--- tests/calibration/test_dotty_grid_detector_metal.py
+++ tests/calibration/test_dotty_grid_detector_metal.py
@@ -31,7 +31,7 @@ def test_metal_1a(setup_dotty_metal_model_OR):
                                                   'tests/data/calib-ucl-circles/10_54_44/viking.calib.left.distortion.txt'
                                                   )
 
-    assert (number_of_points > 342 and number_of_points < 345)
+    assert (number_of_points > 342 and number_of_points < 354)
 
 
 def test_metal_2(setup_dotty_metal_model):
